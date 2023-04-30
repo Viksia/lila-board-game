92d3ea3c79e2ade03535cd558aba921035c90580
Viktoriia Boiko <boiko.viktorika@gmail.com>
2023-04-30 14:43:05 +0200

    Added 8avatars and changed the dice

--- src/dice.js
+++ src/dice.js
@@ -3,18 +3,21 @@ window.addEventListener(
 	function () {
 		const buttonRoolDice = document.querySelector(".dice-roll");
 
+		function resetDice() {
+			const diceSide1 = document.getElementById("dice-side");
+			diceSide1.innerHTML = "0";
+		}
+
 		function rollDice() {
 			const diceSide1 = document.getElementById("dice-side");
 
-			const status = document.getElementById("status");
-
 			const side1 = Math.floor(Math.random() * 6) + 1;
-
 			const diceTotal = side1;
 
 			diceSide1.innerHTML = side1;
 
-			status.innerHTML = "You rolled " + diceTotal + ".";
+			// Устанавливаем таймер для сброса значения кубика через 7 секунд
+			setTimeout(resetDice, 6000);
 		}
 
 		buttonRoolDice.addEventListener("click", rollDice, false);
